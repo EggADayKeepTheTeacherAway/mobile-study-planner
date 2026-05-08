e4d7e6de6a4028bb8581d47c395696027bae8b85
0CreepySmile0 <yutivich2005@gmail.com>
2026-05-08 16:25:33 +0700

    feat: Create entities

---
+++ app/src/main/java/org/classapp/studyplanner/data/local/entity/Course.kt
@@ -0,0 +1,14 @@
+package org.classapp.studyplanner.data.local.entity
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "courses")
+data class Course(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val courseName: String,
+    val courseCode: String,
+    val courseColor: String = "#1dafa1"
+)
